35670ec6d33dd195acae080a56ce054942e18f31
jesseilev <jesseilev@gmail.com>
2016-04-17 11:07:50 -0500

    add js and css;  write some TODO comments

---
+++ js/flicklist.js
@@ -0,0 +1,28 @@
+
+
+var model = {
+  watchlistItems: [],
+  discoverItems: []
+}
+
+
+/**
+ * Makes an AJAX request to themoviedb.org, asking for some movies
+ * if successful, updates the model.discoverItems appropriately
+ */
+function fetchMovies() {
+  // TODO
+}
+
+
+/**
+ * re-renders the page with new content, based on the current state of the model
+ */
+function render() {
+  // TODO
+}
+
+
+
+
+console.log("if you're seeing this, that means the script loaded!");
