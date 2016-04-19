16e652bb227a221c683792103a1e8b4e7bb619d7
jesseilev <jesseilev@gmail.com>
2016-04-19 11:29:18 -0500

    implement solution, fix comments

--- js/flicklist.js
+++ js/flicklist.js
@@ -1,26 +1,75 @@
 
 
-var api = {
-	root: "https://api.themoviedb.org/3",
-	token: "" // TODO put your api key here
+var model = {
+  watchlistItems: [],
+  browseItems: []
 }
+
+
+var api = {
+  root: "https://api.themoviedb.org/3",
+  token: "8e888fa39ec243e662e1fb738c42ae99"
+}
+
 
 /**
  * Makes an AJAX request to themoviedb.org, asking for some movies
- * if successful, prints the results to the console
+ * if successful, updates the model.browseItems appropriately, and then invokes
+ * the callback function that was passed in
  */
-function testTheAPI() {
-	$.ajax({
-		url: api.root + "/discover/movie",
-		data: {
-			api_key: api.token,
-		},
-		success: function(response) {
-			console.log("We got a response from The Movie DB!");
-			console.log(response);
-		}
-	});
+function discoverMovies(callback) {
+  // DONE
+  
+  $.ajax({
+    url: api.root + "/discover/movie",
+    data: {
+      api_key: api.token
+    },
+    success: function(response) {
+      console.log(response);
+      model.browseItems = response.results;
+      callback(response);
+    },
+    fail: function() {
+      console.log("fail!");
+    }
+  });
 }
 
 
-console.log("The script loaded!");
+/**
+ * re-renders the page with new content, based on the current state of the model
+ */
+function render() {
+  // DONE
+
+  var watchlistElement = $("#section-watchlist ul");
+  var browseElement = $("#section-browse ul");
+
+  // clear everything
+  watchlistElement.empty();
+  browseElement.empty();
+
+  // insert watchlist items
+  model.watchlistItems.forEach(function(movie) {
+    var title = $("<p></p>").text(movie.original_title);
+    var itemView = $("<li></li>").append(title);
+    watchlistElement.append(itemView);
+  });
+
+  // insert browse items
+  model.browseItems.forEach(function(movie) {
+    var title = $("<p></p>").text(movie.original_title);
+    var button = $("<button></button>")
+      .text("Add to Watchlist")
+      .click(function() {
+        model.watchlistItems.push(movie);
+        render();
+      });
+
+    var itemView = $("<li></li>").append(title).append(button);
+    browseElement.append(itemView);
+  });
+  
+}
+
