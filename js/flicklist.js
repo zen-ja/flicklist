6efdccb21965b8e9a2b77c12005629e41a933b9c
jesseilev <jesseilev@gmail.com>
2016-05-03 12:07:38 -0500

    add todos

--- js/flicklist.js
+++ js/flicklist.js
@@ -8,7 +8,16 @@ var model = {
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "8e888fa39ec243e662e1fb738c42ae99" // TODO 0 (DONE) add your api key
+  token: "8e888fa39ec243e662e1fb738c42ae99", // TODO 0 (DONE) add your api key
+
+  /**
+   * Given a movie object, return the url to its poster image
+   */
+  posterUrl: function(movie) {
+    // TODO
+    // implement this function
+    
+  }
 }
 
 
@@ -39,8 +48,6 @@ function discoverMovies(callback) {
  * the callback function that was passed in
  */
 function searchMovies(query, callback) {
-  // TODO 8 (DONE)
-
   $.ajax({
     url: api.root + "/search/movie",
     data: {
@@ -66,18 +73,33 @@ function render() {
 
   // insert watchlist items
   model.watchlistItems.forEach(function(movie) {
-    var title = $("<p></p>").text(movie.original_title);
+    var title = $("<h6></h6>").text(movie.original_title);
+
+    // TODO 
+    // add an "I watched it" button
+
+    // TODO 
+    // add a poster image
+
+    // TODO 
+    // re-implement the li as a bootstrap panel
+
     var itemView = $("<li></li>")
       .append(title)
       .attr("class", "item-watchlist");
-      // TODO 3 (DONE see line above)
-      // give itemView a class attribute of "item-watchlist"
 
     $("#section-watchlist ul").append(itemView);
   });
 
   // insert browse items
   model.browseItems.forEach(function(movie) {
+
+    // TODO
+    // style this list item to look like the demo
+    // You'll also need to make changes in index.html.
+    // use the following BS classes:
+    // "list-group", "list-group-item", btn", "btn-primary", 
+
     var title = $("<h4></h4>").text(movie.original_title);
     var button = $("<button></button>")
       .text("Add to Watchlist")
@@ -86,21 +108,11 @@ function render() {
         render();
       })
       .prop("disabled", model.watchlistItems.indexOf(movie) !== -1);
-      // TODO 2 (DONE see line above)
-      // the button should be disabled if this movie is already in
-      // the user's watchlist
-      // see jQuery .prop() and Array.indexOf()
 
-
-    // TODO 1 (DONE)
-    // create a paragraph containing the movie object's .overview value
-    // then append the paragraph in between the title and the button
     var overview = $("<p></p>").text(movie.overview);
-
 
     // append everything to itemView, along with an <hr/>
     var itemView = $("<li></li>")
-      .append($("<hr/>"))
       .append(title)
       .append(overview)
       .append(button);
@@ -112,5 +124,8 @@ function render() {
 }
 
 
-
-
+// When the HTML document is ready, we call the discoverMovies function,
+// and pass the render function as its callback
+$(document).ready(function() {
+  discoverMovies(render);
+});
