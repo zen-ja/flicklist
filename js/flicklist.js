3a335ec8c92b61e5f27f4cce3f8caa74d706731c
jesseilev <jesseilev@gmail.com>
2016-05-10 13:36:59 -0500

    remove my key

--- js/flicklist.js
+++ js/flicklist.js
@@ -8,7 +8,7 @@ var model = {
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "8e888fa39ec243e662e1fb738c42ae99", // TODO 0 add your api key
+  token: "TODO", // TODO 0 add your api key
   /**
    * Given a movie object, returns the url to its poster image
    */
