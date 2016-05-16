aa7d8c3dbdfe0b41e20b8942040c3cf9cae299ec
jesseilev <jesseilev@gmail.com>
2016-05-16 14:08:56 -0500

    mark todos as done, delete old code

--- js/flicklist.js
+++ js/flicklist.js
@@ -155,28 +155,3 @@ $(document).ready(function() {
   discoverMovies(render);
 });
 
-
-
-
-// // render browse items
-//   model.browseItems.forEach(function(movie) {
-//     var title = $("<h4></h4>").text(movie.original_title);
-//     var overview = $("<p></p>").text(movie.overview);
-
-//     // button for adding to watchlist
-//     var button = $("<button></button>")
-//       .text("Add to Watchlist")
-//       .attr("class", "btn btn-primary")
-//       .click(function() {
-//         model.watchlistItems.push(movie);
-//         render();
-//       })
-//       .prop("disabled", model.watchlistItems.indexOf(movie) !== -1);
-
-//     var itemView = $("<li></li>")
-//       .attr("class", "list-group-item")
-//       .append( [title, overview, button] );
-      
-//     // append the itemView to the list
-//     $("#section-browse ul").append(itemView);
-//   });
